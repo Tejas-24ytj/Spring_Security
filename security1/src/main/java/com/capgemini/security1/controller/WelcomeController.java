ce7e6c7b4674e5dbabbdb2c7aee31768a99c1994
Tejas <tblamkhade24@gmail.com>
2025-05-05 15:06:29 +0530

    Files committed

---
+++ security1/src/main/java/com/capgemini/security1/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.capgemini.security1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WelcomeController {
+	@GetMapping
+	public String welcomeMessage() {
+	return "Welcome to Spring Boot Security !!";
+	}
+}
